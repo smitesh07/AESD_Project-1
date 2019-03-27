e42dd732f98131e0cfc630cafbee66465562f869
Smitesh Modak <smitesh.modak@colorado.edu>
2019-03-26 18:25:59 -0600

    Moved Makefile to main folder

---
+++ sources.mk
@@ -0,0 +1,12 @@
+#Defining common source files to be used
+SRCS = src/main.c \
+       src/log.c  \
+	   src/timer.c \
+	   src/tempSensor.c\
+       src/queue.c \
+	   src/lumSensor.c \
+	   src/i2c.c \
+	   src/tmp102.c
+
+#Defining paths for the header files
+INCLUDE_H = includes/
